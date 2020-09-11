ef44bbe91fcfa9e37c3cd1a1ac7ab588fb531f9d
ViktorFriholm <71007248+ViktorFriholm@users.noreply.github.com>
2020-09-11 22:03:07 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,3 @@
-// Laboration 1: FizzBuzz
-// Av: Viktor Friholm
-// Uppgiften går ut på att skriva ut alla tal mellan 1 och 100, ett tal per rad.
-// Om talet är jämnt delbart med 3 så ska ordet 'Fizz' skrivas ut istället för talet.
-// Om talet är jämnt delbart med 5 så ska ordet 'Buzz' skrivas ut istället för talet.
-// Om talet är jämnt delbart med både 3 och 5 så ska ordet 'FizzBuzz' skrivas ut istället för någonting annat.
-// Om talet är 42 så ska 'Answer to the Ultimate Question of Life, the Universe, and Everything' skrivas ut.
-
 for(var i= 1; i < 101; i++ ){
     if(i === 42){
         console.log("Answer to the Ultimate Question of Life, the Universe, and Everything");
